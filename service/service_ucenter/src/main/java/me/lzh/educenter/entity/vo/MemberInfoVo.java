7a824bdb023ec7d0bf5db45bc86b2cd10c09f739
EB9.LU <5174291+eb9@user.noreply.gitee.com>
2020-08-23 13:22:02 +0800

    ucenter

---
+++ service/service_ucenter/src/main/java/me/lzh/educenter/entity/vo/MemberInfoVo.java
@@ -0,0 +1,48 @@
+package me.lzh.educenter.entity.vo;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Created by lizihao on 2020/8/9 15:47
+ */
+@Data
+public class MemberInfoVo {
+
+
+    @ApiModelProperty(value = "会员id")
+    @TableId(value = "id", type = IdType.ID_WORKER_STR)
+    private String id;
+
+    @ApiModelProperty(value = "微信openid")
+    private String openid;
+
+    @ApiModelProperty(value = "手机号")
+    private String mobile;
+
+
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "性别 1 女，2 男")
+    private Integer sex;
+
+    @ApiModelProperty(value = "年龄")
+    private Integer age;
+
+    @ApiModelProperty(value = "用户头像")
+    private String avatar;
+
+    @ApiModelProperty(value = "用户签名")
+    private String sign;
+
+
+
+
+}
---
+++ service/service_ucenter/src/main/java/me/lzh/educenter/mapper/UcenterMemberMapper.java
@@ -0,0 +1,16 @@
+package me.lzh.educenter.mapper;
+
+import me.lzh.educenter.entity.UcenterMember;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author codeGenerator
+ * @since 2020-08-09
+ */
+public interface UcenterMemberMapper extends BaseMapper<UcenterMember> {
+
+}
